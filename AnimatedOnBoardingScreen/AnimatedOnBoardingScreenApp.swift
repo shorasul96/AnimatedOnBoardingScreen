54c1defd490e698cb4c403d3438dd60073146eb3
Shorasul Sh <shoazimovshorasul@gmail.com>
2022-12-20 03:21:52 +0500

    Initial Commit

---
+++ AnimatedOnBoardingScreen/AnimatedOnBoardingScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedOnBoardingScreenApp.swift
+//  AnimatedOnBoardingScreen
+//
+//  Created by Shorasul Sh on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedOnBoardingScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
