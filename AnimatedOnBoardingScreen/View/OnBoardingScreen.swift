1ac66c78fc58d5039e6bdec7cbaa33c2c8165fa3
Shorasul Sh <shoazimovshorasul@gmail.com>
2022-12-21 00:13:31 +0500

    Added: lotties & button next/back

--- AnimatedOnBoardingScreen/View/OnBoardingScreen.swift
+++ AnimatedOnBoardingScreen/View/OnBoardingScreen.swift
@@ -10,40 +10,163 @@ import Lottie
 
 struct OnBoardingScreen: View {
     @State var onboardingItems: [OnboardingItem] = [
-        .init(title: "My test lorem",
-                subTitle: "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..",
-                lottiView: .init(name: "28595-website-building-lottie-animation", bundle: .main)),
-        .init(title: "Lorem Ipsum",
-                subTitle: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
-                lottiView: .init(name: "51083-website-interface", bundle: .main)),
-        .init(title: "HODOR IPSUM",
-                subTitle: "If you haven't seen Game of Thrones, go watch it right now. If you have then you'll totally get why this Hodor themed lorem ipsum generator is just brilliant.",
-                lottiView: .init(name: "57081-graphics-design-lottie-animation", bundle: .main)),
+        .init(title: "Lorem ipsum dolor sit amet",
+              subTitle: "Est dolor quasi est magnam deleniti aut obcaecati dolor. Vel impedit omnis non nostrum quia est omnis deserunt in odio voluptas. Et esse nihil aut provident nesciunt ut cupiditate laudantium et minus neque.",
+              lottiView: .init(name: "sad-walking", bundle: .main)),
+        .init(title: "Lorem ipsum dolor sit amet",
+              subTitle: "Est dolor quasi est magnam deleniti aut obcaecati dolor. Vel impedit omnis non nostrum quia est omnis deserunt in odio voluptas. Et esse nihil aut provident nesciunt ut cupiditate laudantium et minus neque.",
+              lottiView: .init(name: "we-are-sorry", bundle: .main)),
+        .init(title: "Lorem ipsum dolor sit amet",
+              subTitle: "Est dolor quasi est magnam deleniti aut obcaecati dolor. Vel impedit omnis non nostrum quia est omnis deserunt in odio voluptas. Et esse nihil aut provident nesciunt ut cupiditate laudantium et minus neque.",
+              lottiView: .init(name: "happy-baby", bundle: .main))
     ]
+    
+    @State var currentIndex: Int = 0
     var body: some View {
         GeometryReader {
             let size = $0.size
-
+            
             HStack(spacing: 0) {
-                ForEach(onboardingItems) { item in
+                ForEach($onboardingItems) { $item in
+                    let isLastSlide = currentIndex == onboardingItems.count - 1
                     VStack {
-                        VStack {
+                        //MARK: Top nav bar
+                        HStack{
+                            Button("Back"){
+                                if currentIndex > 0 {
+                                    currentIndex -=  1
+                                    playAnimation()
+                                }
+                                
+                            }
+                            .opacity(currentIndex > 0 ? 1 : 0)
+                            Spacer(minLength: 0)
+                        }
+                        .tint(Color.secondary)
+                        .fontWeight(.semibold)
+                        
+                        //MARK: Movable Slide
+                        VStack(spacing: 15) {
+                            let offset = -CGFloat(currentIndex) * size.width
+                            
+                            //MARK: Resizable Lottie view
+                            ResizableLottiView(onboardingItem: $item)
+                                .frame(height: size.width)
+                                .onAppear{
+                                    //MARK: Initially playing the first lottie
+                                    if(currentIndex == indexOf(item)){
+                                        item.lottiView.play(toProgress: 0.9)
+                                    }
+                                }
+                                .offset(x: offset)
+                                .animation(.easeOut(duration: 0.5), value: currentIndex)
+                            
                             Text(item.title)
                                 .font(.title.bold())
+                                .offset(x: offset)
+                                .animation(.easeOut(duration: 0.5).delay(0.1), value: currentIndex)
+                            
+                            Text(item.subTitle)
+                                .font(.system(size: 14))
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(.gray)
+                                .offset(x: offset)
+                                .animation(.easeOut(duration: 0.5).delay(0.2), value: currentIndex)
+                            
                         }
+                        
+                        Spacer(minLength: 0)
+                        
+                        //MARK: Next / Login Button
+                        VStack(spacing: 15){
+                            Text(isLastSlide ? "Happy end" : "Next")
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                                .padding(.vertical,12)
+                                .frame(maxWidth: .infinity)
+                                .background{
+                                    Capsule()
+                                        .fill(Color(uiColor: .black))
+                                }
+                                .padding(.horizontal, isLastSlide ? 30 : 100)
+                                .onTapGesture{
+                                    //MARK: update to the next index
+                                    if currentIndex < onboardingItems.count - 1 {
+                                        onboardingItems[currentIndex].lottiView.pause()
+                                        currentIndex += 1
+                                        playAnimation()
+                                    }
+                                    
+                                }
+                            
+                            HStack{
+                                Text("Term of Momoxa")
+                                Text("Privacy of Nurangiz")
+                                
+                            }
+                            .font(.caption2)
+                            .underline(true, color: .primary)
+                            .offset(y: 5)
+                            
+                        }
+                        
                     }
-                            .padding(15)
-                            .frame(width: size.width, height: size.height)
+                    .animation(.easeOut, value: isLastSlide)
+                    .padding(14)
+                    .frame(width: size.width, height: size.height)
                 }
             }
-                    .frame(width: size.width * CGFloat(onboardingItems.count), alignment: .leading)
-
+            .frame(width: size.width * CGFloat(onboardingItems.count), alignment: .leading)
+            
         }
+    }
+    
+    func playAnimation(){
+        onboardingItems[currentIndex].lottiView.currentProgress = 0
+        onboardingItems[currentIndex].lottiView.play(fromProgress: 0,  toProgress: 1,
+                                                     loopMode: LottieLoopMode.loop)
+    }
+    
+    //MARK: retrieving index of items
+    func indexOf(_ item: OnboardingItem) -> Int{
+        if let index = onboardingItems.firstIndex(of: item){
+            return index
+        }
+        return 0
     }
 }
 
 struct OnBoardingScreen_Previews: PreviewProvider {
     static var previews: some View {
         OnBoardingScreen()
+    }
+}
+
+struct ResizableLottiView: UIViewRepresentable{
+    @Binding var onboardingItem: OnboardingItem
+    func makeUIView(context: Context) -> some UIView {
+        let view = UIView()
+        view.backgroundColor = .clear
+        setupLottieView(view)
+        
+        return view
+    }
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
+    func setupLottieView(_ to: UIView){
+        let lottieView =  onboardingItem.lottiView
+        lottieView.backgroundColor = .clear
+        lottieView.translatesAutoresizingMaskIntoConstraints = false
+        
+        //MARK: Applying Constrains
+        let constrains = [
+            lottieView.widthAnchor.constraint(equalTo: to.widthAnchor),
+            lottieView.heightAnchor.constraint(equalTo: to.heightAnchor)
+        ]
+        to.addSubview(lottieView)
+        to.addConstraints(constrains)
+        
     }
 }
